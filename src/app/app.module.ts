e431cd2cae11f94242b4b3c992dcbfd7c2500c15
Doug <douglaswleonard+git@gmail.com>
2018-06-27 13:50:04 -0600

    add identity component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IdentityComponent } from './identity/identity.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdentityComponent
   ],
   imports: [
     BrowserModule
