269a818228c516586bbb72bcdadc645b9bd30fdd
Doug <douglaswleonard+git@gmail.com>
2018-06-27 15:13:48 -0600

    add message components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { IdentityComponent } from './identity/identity.component';
 import { ChannelMembersComponent } from './channel-members/channel-members.component';
+import { MessageBoxComponent } from './message-box/message-box.component';
+import { MessageComponent } from './message/message.component';
+import { MessageInputComponent } from './message-input/message-input.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     IdentityComponent,
-    ChannelMembersComponent
+    ChannelMembersComponent,
+    MessageBoxComponent,
+    MessageComponent,
+    MessageInputComponent
   ],
   imports: [
     BrowserModule,
